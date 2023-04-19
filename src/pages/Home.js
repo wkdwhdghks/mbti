c566315fb9f4b246309fc5ef2b1f8998996d129d
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-04-19 17:56:45 +0900

    mbti 필터기능 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,30 @@
+import { Link } from "react-router-dom";
+import mock from "../mock.json";
+import { useState } from "react";
+
 export default function Home() {
-  return <div>Home</div>;
+  const { results } = mock;
+  const [filter, setFilter] = useState(null);
+
+  return (
+    <div>
+      <h1>MBTI별 좋아하는 컬러</h1>
+      <div>
+        <Link to="/new">+ 새 컬러 등록하기</Link>
+        {filter && (
+          <div onClick={() => setFilter(null)}>
+            {filter}
+            <img src="/images/_x.svg" alt="필터 삭제" />
+          </div>
+        )}
+        <ul>
+          {results.map((item) => (
+            <li key={item.id} onClick={() => setFilter(item.mbti)}>
+              {item.id} {item.mbti} --- {item.colorCode}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
