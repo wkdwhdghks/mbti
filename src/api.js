0b8ba98613658356c92195577a75158d81f62b44
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-04-19 16:35:22 +0900

    프로젝트 초기화

--- src/api.js
+++
@@ -1,6 +0,0 @@
-export async function getFoods({ order = "", cursor = "", limit = 10 }) {
-  const query = `order=${order}&cursor=${cursor}&limit=${limit}`;
-  const response = await fetch(`https://learn.codeit.kr/3514/foods?${query}`);
-  const body = await response.json();
-  return body;
-}
