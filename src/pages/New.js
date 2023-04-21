d759edb6902d7d0eb974fcd618ef2203a32682e2
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-04-21 15:02:22 +0900

    랜덤 색상 코드 생성하기

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
-import MBTISelect from "../components/MBTISelect";
 import { useState } from "react";
+import MBTISelect from "../components/MBTISelect";
+import generateColorCode from "../lib/generateColorCode";
 
 export default function New() {
   const [formValue, setFormValue] = useState({
@@ -15,6 +16,18 @@ export default function New() {
     }));
   };
 
+  function handleRandomClick() {
+    const nextColorCode = generateColorCode();
+    handleChange("colorCode", nextColorCode);
+  }
+
+  function handleColorCodeBlur() {
+    const isValidColorCode = /^#[a-f0-9]{6}$/i.test(formValue.colorCode);
+    if (!isValidColorCode) {
+      handleChange("colorCode", "#000000");
+    }
+  }
+
   return (
     <div>
       <h1>새 컬러 등록하기</h1>
@@ -22,16 +35,15 @@ export default function New() {
         <img src="/images/_x.svg" alt="취소" />
       </Link>
       <h2>MBTI</h2>
-
       <MBTISelect
         value={formValue.mbti}
         onChange={(newMbti) => handleChange("mbti", newMbti)}
       />
-
       <h2>컬러</h2>
-      <img src="/images/repeat.svg" alt="랜덤" />
+      <img src="/images/repeat.svg" alt="랜덤" onClick={handleRandomClick} />
       <input
         value={formValue.colorCode}
+        onBlur={handleColorCodeBlur}
         onChange={(e) => handleChange("colorCode", e.target.value)}
       />
       <button>컬러 등록</button>
